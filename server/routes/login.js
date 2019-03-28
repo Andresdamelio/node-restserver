3ad79190166a566a5caf2a4f8771d91d58906c7c
AndresDamelio <andresdamelio24@gmail.com>
2019-03-28 12:23:32 -0400

    nuevos cambios

--- server/routes/login.js
+++ server/routes/login.js
@@ -9,9 +9,15 @@ const app = express();
 
 app.post('/login', (req, res) => {
 
+    /* Se obtiene el cuerpo de la petición para tomar el correo y la contraseña ingresada */
+
     let body = req.body;
 
+    /* usuario.findOne devuelve un solo usuario, en este caso se le pasa la condicion de que se email sea el ingresado, y el callback con el error o el usuario */
+
     usuario.findOne({ email: body.email }, (err, usuarioDB) => {
+
+        /* Se verifica si no ocurrió algún error con la base de datos o el servdor, y se envia un error 500 en caso de que exista  */
 
         if(err){
             return res.status(500).json({
@@ -19,6 +25,8 @@ app.post('/login', (req, res) => {
                 err
             })
         }
+
+        /* Se verifica si el usuario ingresado existe, en caso de no existir se envia el error 400 */
 
         if ( !usuarioDB ) {
             return res.status(400).json({
@@ -29,6 +37,8 @@ app.post('/login', (req, res) => {
             })
         }
 
+        /* Se verifica que la contraseña ingresada coincida con la que esta guardada en la base de datos, en este caso se utiliza bcrypt.compareSync que permite comparar las dos contraseñas, este mismo se encarga de encriptar la ingresada y compararla con la existente. */
+
        if( !bcrypt.compareSync( body.password, usuarioDB.password )){
         return res.status(400).json({
             ok:false,
@@ -37,6 +47,8 @@ app.post('/login', (req, res) => {
             }
         })
        }
+
+       /* En caso de que no ocurra ningún incoveniente, se envia la respuesta positiva, con un estatus 200, ademas se envia el usuario y el token de acceso */
 
        res.json({
             ok:true,
