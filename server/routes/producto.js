d7004f6a5528f63f13f586ff85ba69b44be59350
AndresDamelio <andresdamelio24@gmail.com>
2019-05-01 15:09:24 -0400

    obtener todos los productos, con paginacion

--- server/routes/producto.js
+++ server/routes/producto.js
@@ -5,12 +5,46 @@ const { verificationToken } = require('../middlewares/autenticacion');
 const Producto = require('../models/producto');
 
 /* Obtener todos los productos */
-app.get('/producto', (req,res)=>{
+app.get('/producto', verificationToken, (req,res)=>{
+
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    Producto.find({})
+        .populate('categoria', 'descripcion')
+        .populate('usuario', 'nombre, email')
+        .skip(desde)
+        .limit(limite)
+        .exec( (err, productos) => {
+            if( err ){
+                return res.status(500).json({
+                    ok:false,
+                    err
+                })
+            }
+
+            if( !productos ){
+                res.status(400).json({
+                    ok:false,
+                    err: {
+                        message: 'No hay productos disponibles'
+                    }
+                })
+            }
+
+            res.json({
+                ok:true,
+                productos
+            })
+        })
     /* Traer todos los productos, populate de usuario y categoria y paginacion */
 })
 
 /* Obtener un prodcuto por id */
-app.get('/producto/:id', (req,res)=>{
+app.get('/producto/:id', verificationToken, (req,res)=>{
     /* populate */
 })
 
@@ -56,12 +90,12 @@ app.post('/producto', verificationToken, (req, res)=>{
 })
 
 /*  Modificar un producto por id */
-app.put('/producto/:id', (req,res)=>{
+app.put('/producto/:id', verificationToken, (req,res)=>{
     /* populate */
 })
 
 /* Eliminar un producto por id, (eliminacion logica) */
-app.delete('/producto/:id', (req,res)=>{
+app.delete('/producto/:id', verificationToken, (req,res)=>{
     /* populate */
 })
 
